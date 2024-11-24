a85c70d667d6a84565551a5d9023dcf3c5e641fb
VinhNguyen <vinh@cck.co.jp>
2024-11-24 13:11:11 +0700

    [Implement] Members functionality

--- src/app/workspace/[workspaceId]/workspace-switcher.tsx
+++ src/app/workspace/[workspaceId]/workspace-switcher.tsx
@@ -20,7 +20,7 @@ export const WorkspaceSwitcher = () => {
 
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger>
+      <DropdownMenuTrigger asChild>
         <Button className="size-9 relative overflow-hidden bg-[#ababad] hover:bg-[#ababad]/80 text-slate-800 font-semibold text-xl">
           {
             workspaceLoading ? (
