0f59640513160be36aa96c2dec04d3a3aef268d3
VinhNguyen <vinh@cck.co.jp>
2025-01-08 22:51:01 +0700

    [Implement] Messages list component

--- src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
+++ src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
@@ -5,6 +5,7 @@ import { Loader, TriangleAlert } from "lucide-react";
 import { Header } from "./header";
 import { ChatInput } from "./chat-input";
 import { useChannelId } from "@/hooks/use-channel-id";
+import { MessageList } from "@/components/message-list";
 
 import { UseGetChannel } from "@/app/features/channels/api/use-get-channel";
 import { useGetMessages } from "@/app/features/messages/api/use-get-messages";
@@ -12,10 +13,10 @@ import { useGetMessages } from "@/app/features/messages/api/use-get-messages";
 const ChannelIdPage = () => {
   const channelId = useChannelId();
 
-  const { results } = useGetMessages({ channelId })
+  const { results, status, loadMore } = useGetMessages({ channelId })
   const { data: channel, isLoading: channelLoading } = UseGetChannel({ id: channelId });
 
-  if (channelLoading) {
+  if (channelLoading || status === 'LoadingFirstPage') {
     return (
       <div className="h-full flex items-center justify-center flex-1">
         <Loader className="size-5 animate-spin text-muted-foreground" />
@@ -37,10 +38,14 @@ const ChannelIdPage = () => {
   return (
     <div className="flex flex-col h-full">
       <Header title={channel.name} />
-      <div className="flex-1">
-        {JSON.stringify(results)}
-      </div>
-
+      <MessageList
+        channelName={channel.name}
+        channelCreationTime={channel._creationTime}
+        data={results}
+        loadMore={loadMore}
+        isLoadingMore={status === 'LoadingMore'}
+        canLoadMore={status === 'CanLoadMore'}
+      />
       <ChatInput placeholder={`Message # ${channel.name}`} />
     </div>
   );
---
+++ src/components/message-list.tsx
@@ -0,0 +1,105 @@
+import { differenceInMinutes, format, isToday, isYesterday } from 'date-fns'
+
+import { Message } from './message';
+import { GetMessagesReturnType } from "@/app/features/messages/api/use-get-messages";
+
+const TIME_THRESHOLD = 5;
+
+interface MessageListProps {
+  memberName?: string;
+  memberImage?: string;
+  channelName?: string;
+  channelCreationTime?: number;
+  variant?: 'channel' | 'thread' | 'conversation';
+  data: GetMessagesReturnType | undefined;
+  loadMore: () => void;
+  isLoadingMore: boolean;
+  canLoadMore: boolean;
+}
+
+const formatDateLabel = (dateStr: string) => {
+  const date = new Date(dateStr)
+
+  if (isToday(date)) return 'Today'
+  if (isYesterday(date)) return 'Yesterday'
+
+  return format(date, 'EEEE, MMMM d')
+}
+
+export const MessageList = ({
+  memberName,
+  memberImage,
+  channelName,
+  channelCreationTime,
+  variant = 'channel',
+  data,
+  loadMore,
+  isLoadingMore,
+  canLoadMore
+}: MessageListProps) => {
+  const groupedMessage = data?.reduce((groups, message) => {
+    const date = new Date(message._creationTime)
+    const dateKey = format(date, 'yyyy-MM-dd')
+
+    if (!groups[dateKey]) {
+      groups[dateKey] = [];
+    }
+
+    groups[dateKey].unshift(message)
+
+    return groups
+  },
+    {} as Record<string, typeof data>
+  )
+
+  return (
+    <div className="flex-1 flex flex-col-reverse pb-4 overflow-y-auto messages-scrollbar">
+      {
+        Object.entries(groupedMessage || {}).map(([dateKey, messages]) => (
+          <div key={dateKey}>
+            <div className='text-center my-2 relative'>
+              <hr className='absolute top-1/2 left-0 right-0 border-t border-gray-300' />
+              <span className='relative inline-block bg-white px-4 py-1 rounded-full text-xs border border-gray-300 shadow-sm'>
+                {formatDateLabel(dateKey)}
+              </span>
+            </div>
+
+            {messages.map((message, index) => {
+              const prevMessage = messages[index - 1];
+              const isCompact =
+                prevMessage &&
+                prevMessage.user?._id === message.user?._id &&
+                differenceInMinutes(
+                  new Date(message._creationTime),
+                  new Date(prevMessage._creationTime)
+                ) < TIME_THRESHOLD
+
+              return (
+                <Message
+                  key={message._id}
+                  id={message._id}
+                  memberId={message.memberId}
+                  authorImage={message.user.image}
+                  authorName={message.user.name}
+                  isAuthor={false}
+                  reactions={message.reactions}
+                  body={message.body}
+                  image={message.image}
+                  updatedAt={message.updatedAt}
+                  createdAt={message._creationTime}
+                  isEditing={false}
+                  setEditingId={() => { }}
+                  isCompact={isCompact}
+                  hideThreadButton={false}
+                  threadCount={message.threadCount}
+                  threadImage={message.threadImage}
+                  threadTimestamp={message.threadTimestamp}
+                />
+              )
+            })}
+          </div>
+        ))
+      }
+    </div>
+  )
+}
