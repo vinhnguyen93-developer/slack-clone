0e09d8402e485ac9da98fa1e6c7f00817c157a34
VinhNguyen <vinh@cck.co.jp>
2025-01-14 22:28:00 +0700

    [Implement] Message toolbar component

--- src/components/message.tsx
+++ src/components/message.tsx
@@ -2,6 +2,7 @@ import dynamic from "next/dynamic";
 import { format, isToday, isYesterday } from "date-fns";
 
 import { Hint } from "./hint";
+import { Toolbar } from "./toolbar";
 import { Thumbnail } from "./thumbnail";
 import { Doc, Id } from "../../convex/_generated/dataModel";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
@@ -111,6 +112,17 @@ export const Message = ({
           ) : null}
         </div>
       </div>
+      {!isEditing && (
+        <Toolbar
+          isAuthor={isAuthor}
+          isPending={false}
+          handleEdit={() => setEditingId(id)}
+          handleThread={() => { }}
+          handleDelete={() => { }}
+          handleReaction={() => { }}
+          hideThreadButton={hideThreadButton}
+        />
+      )}
     </div>
   )
 }
