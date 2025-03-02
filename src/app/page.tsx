0237fe41775ad03af46a61c589f6972c6ae53db8
VinhNguyen <vinh@cck.co.jp>
2025-03-02 17:12:01 +0700

    [Fix] Loading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
 'use client'
 
+import { Loader } from "lucide-react"
 import { useEffect, useMemo } from "react"
 import { useRouter } from "next/navigation"
-
-import { UserButton } from "./features/auth/components/user-button"
 
 import { useGetWorkspaces } from "./features/workspaces/api/use-get-workspaces"
 import { useCreateWorkspaceModal } from "./features/workspaces/store/use-create-workspace-modal"
@@ -27,8 +26,8 @@ export default function Home() {
   }, [workspacesId, isLoading, open, setOpen, router])
 
   return (
-    <div>
-      <UserButton />
+    <div className="h-full flex items-center justify-center">
+      <Loader className="size-6 animate-spin text-muted-foreground" />
     </div>
   )
 }
